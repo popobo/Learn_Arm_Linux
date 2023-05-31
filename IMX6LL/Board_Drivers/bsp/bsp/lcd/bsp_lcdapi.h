5cd26e8ebb6ebab03fe77b717ec7279f2da2620a
popobo <331361996@qq.com>
2023-05-31 23:01:42 +0800

    lcd

---
+++ IMX6LL/Board_Drivers/bsp/bsp/lcd/bsp_lcdapi.h
@@ -0,0 +1,19 @@
+#ifndef BSP_LCDAPI_H
+#define BSP_LCDAPI_H
+
+#include "imx6ul.h"
+#include "bsp_lcd.h"
+
+/* 函数声明 */
+void lcd_drawline(uint16_t x1, uint16_t y1, uint16_t x2, uint16_t y2);
+void lcd_draw_rectangle(uint16_t x1, uint16_t y1, uint16_t x2, uint16_t y2);
+void lcd_draw_Circle(uint16_t x0,uint16_t y0,uint8_t r);
+void lcd_showchar(unsigned     short x,uint16_t y,uint8_t num,uint8_t size, uint8_t mode);
+uint32_t lcd_pow(uint8_t m,uint8_t n);
+void lcd_shownum(uint16_t x, uint16_t y, uint32_t num, uint8_t len,uint8_t size);
+void lcd_showxnum(uint16_t x, uint16_t y, uint32_t num, uint8_t len, uint8_t size, uint8_t mode);
+void lcd_show_string(uint16_t x,uint16_t y,
+uint16_t width, uint16_t height, uint8_t size,char *p);
+
+#endif
+
--- IMX6LL/Board_Drivers/bsp/project/main.c
+++ IMX6LL/Board_Drivers/bsp/project/main.c
@@ -9,6 +9,15 @@
 #include "bsp_keyfilter.h"
 #include "bsp_uart.h"
 #include "stdio.h"
+#include "bsp_lcd.h"
+#include "bsp_lcdapi.h"
+
+/* 背景颜色索引 */
+unsigned int backcolor[10] = {
+	LCD_BLUE, 		LCD_GREEN, 		LCD_RED, 	LCD_CYAN, 	LCD_YELLOW, 
+	LCD_LIGHTBLUE, 	LCD_DARKBLUE, 	LCD_WHITE, 	LCD_BLACK, 	LCD_ORANGE
+
+}; 
 
 /*
  * @description	: mian函数
@@ -26,18 +35,27 @@ int main(void)
     beep_init();
     filterkey_init();
     uart_init();        /* 初始化串口，波特率115200 */
+    lcd_init();         /* 初始化LCD */
 
-    uint8_t state = OFF;
-    uint32_t a = 0;
-    uint32_t b = 0;
+    uint8_t index = 0;
+    uint8_t state = 0;
+
 	while(1)
 	{
-        printf("输入两个整数，使用空格隔开：");
-        scanf("%d %d", &a, &b);
-        printf("\r\n 数据 %d + %d = %d \r\n", a, b, a + b);
-
+        lcd_clear(backcolor[index]);
+        delayms(1);
+        lcd_show_string(10, 40, 260, 32, 32,(char*)"ALPHA IMX6U"); 	
+		lcd_show_string(10, 80, 240, 24, 24,(char*)"RGBLCD TEST");
+		lcd_show_string(10, 110, 240, 16, 16,(char*)"ATOM@ALIENTEK");      					 
+		lcd_show_string(10, 130, 240, 12, 12,(char*)"2019/8/14");
+        index++;
+        if(index == 10)
+        {   
+            index = 0;
+        }
         state = !state;
         led_switch(LED0, state);
+        delayms(1000);
     }
 
 	return 0;
